5ebadf938e3f6ad7a4a886f00b3736948fad8172
Dani khalang <danikhalang@gmail.com>
2024-01-05 18:57:28 +0100

    controler proces modified, device status added

--- Control_units/control_unit_1.py
+++ Control_units/control_unit_1.py
@@ -20,6 +20,7 @@ class Controler():
 
 
         ## Creating and stablishing MQTT client
+        clientID += "_C"   # Controler
         self.client = MyMQTT(clientID, broker, port, self)
         # Connecting to the broker as both publisher and subscriber
         self.client.start()
@@ -55,51 +56,87 @@ class Controler():
 
         # Classifing and analysing the data according to the type of transmiter sensor
         if senKind == "temperature":
-            # Processing the data and getting the command to send to actuators
-            command_msg = self.check_temp_with_catalog(levelID, plantID, event["v"])
-
-            # Sending command to the actuators    
-            self.client.myPublish(command_msg["bn"], command_msg)   
+            # Processing the data, generationg the command and sending to actuators
+            self.send_command(levelID, plantID, event["v"])
+   
 
 
 
 
     # Processing the data for each subtopic(correcponding level and plant unit)
-    def check_temp_with_catalog(self, levelID, plantID, value):
+    def send_command(self, levelID, plantID, value):
         
-        # Getting the kind of plant for the mean level and plant ID
-        plantFound = False
-        for plant in self.plants:
-            if plant["levelID"] == levelID and plant["plantID"] == plantID:
-                plantKindName = plant["plantKind"]
-                plantFound = True
-                break
-        if not plantFound:
-            print(f"Plant not present. levelID: {levelID}, plantID: {plantID}")
-            return
+        # Updating the plant info by a get request to the catalog
+        try:
+            thePlant_req = requests.get(self.catalog_url+"plant/"+str(levelID)+"/"+str(plantID))
+        
+        except requests.exceptions.RequestException as e:
+            return f"Error during GET request for getting the plant: {e}"
+        
+        plant = thePlant_req.json()
+        plantKindName = plant["plantKind"]
+
+        # other way: stororing and updating all the plants each time
+        # plantFound = False
+        # for plant in self.plants:
+        #     if plant["levelID"] == levelID and plant["plantID"] == plantID:
+        #         plantKindName = plant["plantKind"]
+        #         plantFound = True
+        #         break
+        # if not plantFound:
+        #     print(f"Plant not present. levelID: {levelID}, plantID: {plantID}")
+        #     return
         
         # Getting the suitable temperature of the corresponding plant kind
         for plantKindDict in self.plantKinds:
             if plantKindDict["plantKind"] == plantKindName:
-                minTemp, maxTemp = plantKindDict["coldestTemperature"], plantKindDict["hottestTemperature"]
+                minTemp, maxTemp, bestTempRange = plantKindDict["coldestTemperature"], plantKindDict["hottestTemperature"], plantKindDict["bestTemperatureRange"]
+        
+    
+        # Status of the temp actuators of the plant     
+        fan_status, heater_status = None, None
+        for device in plant["devicesList"]:
+            if device["deviceName"] == "fan_switch":
+                fan_status = device["deviceStatus"]
+            
+            elif device["deviceName"] == "heater_switch":
+                heater_status = device["deviceStatus"]
+
+        if not (fan_status and heater_status):
+            print('failed to get the status of the fan and heater')
         
         # Structure the SenML message
         msg = self.msg.copy()
 
         # Assigning the same level and plant ID as the message received to the base name (also the topic)
-        msg["bn"] += f"{levelID}/{plantID}/temperature"
+        msg["bn"] += f"{levelID}/{plantID}/"
+        topic = msg["bn"]
         msg["e"][0]["t"] = str(time.time())
         
         # Check the temperature as regard to the plant suitable temperature
-        if value < minTemp:
-            print("Temperature less than threshold")
+        if value < minTemp and heater_status == "OFF":
+            print("Temperature less than threshold, Turn ON the heater")
+            topic += "heater_switch"
             msg["e"][0]["v"] = "heater on"
-        elif value > maxTemp:
-            print("Temperature more than threshold")
+
+        elif value > maxTemp and fan_status == "OFF":
+            print("Temperature more than threshold, Turn ON the fan")
+            topic += "fan_switch"
             msg["e"][0]["v"] = "cooler on"
         
+        elif value in range(bestTempRange[0], bestTempRange[1]):
+            if heater_status == "ON":
+                topic += "heater_switch"
+                msg["e"][0]["v"] = "OFF"
+            if fan_status == "ON":
+                topic += "fan_switch"
+                msg["e"][0]["v"] = "OFF"
+
         # Returning the message carring on the order
-        return msg
+        if msg["e"][0]["v"]:
+            # Sending command to the actuators    
+            self.client.myPublish(topic, msg)
+            
 
   
 
@@ -111,7 +148,7 @@ class Controler():
         return topic
     
     
-        
+        # self.plant is not needed in current stage
     # Updating the plant lists and plant kinds every 10 minutes
     def periodic_plantList_update(self):
         # Requesting to the catalog
@@ -119,6 +156,7 @@ class Controler():
             req_p = requests.get(self.catalog_url+"plants")
             req_pk = requests.get(self.catalog_url+"plantkinds")
             print("Plant and plant kind lists are updated\n")
+        
         except requests.exceptions.RequestException as e:
             return f"Error during GET request: {e}"
          
