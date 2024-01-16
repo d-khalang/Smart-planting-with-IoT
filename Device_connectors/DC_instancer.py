b4c450fe70fc4c1c36dba1d19a95a3980fbe5853
Dani khalang <danikhalang@gmail.com>
2024-01-16 17:52:10 +0100

    complient with all the sensors

--- Device_connectors/DC_instancer.py
+++ Device_connectors/DC_instancer.py
@@ -6,17 +6,14 @@ if __name__ == "__main__":
     catalog_url = "http://127.0.0.1:8080/"
     # plantConfigFileName = "devices_configuration.json"
     settingFile = "setting.json"
-
-
-    # with open(plantConfigFileName) as fp:
-    #     plantConfig = json.load(fp)
     
     
     # Information needed for creating an Mqtt client
     with open(settingFile) as fp:
         setting = json.load(fp)
+        
     ## DCID_dict -> device connectors' ID and corresponding plant configuration 
-    baseClientID, broker, port , DCID_dict = setting["clientID"], setting["broker"], setting["port"], setting["DCID_dict"]
+    baseClientID, DCID_dict = setting["clientID"], setting["DCID_dict"]
     
     # Dictionary to store instances of device connector
     deviceConnectors = {}
@@ -24,7 +21,7 @@ if __name__ == "__main__":
     # Creating device connectors according to their IDs and registration details of their plant units
     for DCID,plantConfig in DCID_dict.items():
         DC_name = f"raspbery_{DCID}"
-        deviceConnectors["DC_name"] = Device_connector(catalog_url, plantConfig, baseClientID, DCID, broker, port)
+        deviceConnectors[DC_name] = Device_connector(catalog_url, plantConfig, baseClientID, DCID)
 
 
     # Threding can be used for registeration not to delay for sensor data
--- Device_connectors/device_connector.py
+++ Device_connectors/device_connector.py
@@ -1,7 +1,9 @@
 from MyMQTT import MyMQTT
-from sensors import TempSen
+from sensors import TempSen, LightSen, PHSen, WaterLevelSen, TDSSen
 import requests
 import time
+import json
+import copy
 
 ### Defining a general sensor publisher
 class senPublisher():
@@ -24,9 +26,18 @@ class senPublisher():
 
 
 class Device_connector():
-    def __init__(self, catalog_url, plantConfig, baseClientID,DCID, broker, port):
+    def __init__(self, catalog_url, plantConfig, baseClientID,DCID):
         self.catalog_url, self.plantConfig = catalog_url, plantConfig
         clientID = baseClientID+DCID+"_DCS"    # Device connector sensor
+
+        # Requesting to the catalog for broker's information
+        try:
+            broker, port = self.get_broker()
+            
+        except (TypeError, ValueError, requests.exceptions.RequestException, json.JSONDecodeError) as e:
+            print(f"Failed to get the broker's information. Probably server is down. Error:{e}")
+            return
+        
         self.senPublisher = senPublisher(clientID, broker, port)
         self.DATA_SENDING_INTERVAL = 10    # each x seconds get the avg of each second data and send
 
@@ -43,45 +54,104 @@ class Device_connector():
             ]
         }
 
-        ## sensors
+        ## Creating sensor objects
         self.tempSen = TempSen()
+        self.lightSen = LightSen()
+        self.PHSen = PHSen()
+        self.waterLevelSen = WaterLevelSen()
+        self.TDSSen = TDSSen()
 
     # Getting data from 'get_sen_data' and sending it to message broker
     def send_data(self):
         # Get the average data
-        msg = self.get_sen_data()
+        msgTemp, msgLight, msgPH, msgWaterLevel, msgTDS = self.get_sen_data()
 
         # Connect the publisher, publish the message and disconnect
         self.senPublisher.start()
         print("\npublisher started")
         time.sleep(1)
-        self.senPublisher.publish(msg["bn"], msg)
-        print(f"message published on topic: {msg['bn']}")
+        self.senPublisher.publish(msgTemp["bn"], msgTemp)
+        print(f"message {msgTemp['e'][0]['v']} published on topic: {msgTemp['bn']}")
+
+        self.senPublisher.publish(msgLight["bn"], msgLight)
+        print(f"message {msgLight['e'][0]['v']} published on topic: {msgLight['bn']}")
+
+        self.senPublisher.publish(msgPH["bn"], msgPH)
+        print(f"message {msgPH['e'][0]['v']} published on topic: {msgPH['bn']}")
+
+        self.senPublisher.publish(msgWaterLevel["bn"], msgWaterLevel)
+        print(f"message {msgWaterLevel['e'][0]['v']} published on topic: {msgWaterLevel['bn']}")
+
+        self.senPublisher.publish(msgTDS["bn"], msgTDS)
+        print(f"message {msgTDS['e'][0]['v']} published on topic: {msgTDS['bn']}")
+
         time.sleep(1)
         self.senPublisher.stop()
         print("publisher stoped")
 
 
 
-        # for now just tempreture sensor
+        # for now just tempreture sensor, input like  self.tempSen 
     def get_sen_data(self):
-        tempData = []
+        tempData, lightData, PHData, waterLevelData, TDSData = [], [], [], [], []
 
         # Generate a value every second
         for i in range(self.DATA_SENDING_INTERVAL):
             datum = self.tempSen.generate_data()
             tempData.append(datum)
+
+            datum = self.lightSen.generate_data()
+            lightData.append(datum)
+
+            datum = self.PHSen.generate_data()
+            PHData.append(datum)
+
+            datum = self.waterLevelSen.generate_data()
+            waterLevelData.append(datum)
+
+            datum = self.TDSSen.generate_data()
+            TDSData.append(datum)
             time.sleep(1)
         
+        # !!! create a function to get average and change the message
         # Get the mean each 10 second
-        avg = sum(tempData)/len(tempData)
+        avgTemp = sum(tempData)/len(tempData)
+        avgLight = sum(lightData)/len(lightData)
+        avgPH = sum(PHData)/len(PHData)
+        avgWaterLevel = sum(waterLevelData)/len(waterLevelData)
+        avgTDS = sum(TDSData)/len(TDSData)
 
         # Updating the message
-        msg = self.msg.copy()
-        msg['e'][0].update({"n":"temperature", "u":"Cel", "t":str(time.time()), "v":avg})
-        msg["bn"] += "temperature"
+        msgTemp, msgLight, msgPH, msgWaterLevel, msgTDS = copy.deepcopy(self.msg), copy.deepcopy(self.msg), copy.deepcopy(self.msg), copy.deepcopy(self.msg), copy.deepcopy(self.msg),
+        
+        msgTemp['e'][0].update({"n":"temperature", "u":"Cel", "t":str(time.time()), "v":avgTemp})
+        msgTemp["bn"] += "temperature"
 
-        return msg
+        msgLight['e'][0].update({"n":"light", "u":"μmol/m²/s", "t":str(time.time()), "v":avgLight})
+        msgLight["bn"] += "light"
+
+        msgPH['e'][0].update({"n":"PH", "u":"range", "t":str(time.time()), "v":avgPH})
+        msgPH["bn"] += "PH"
+
+        msgWaterLevel['e'][0].update({"n":"waterLevel", "u":"m", "t":str(time.time()), "v":avgWaterLevel})
+        msgWaterLevel["bn"] += "waterLevel"
+
+        msgTDS['e'][0].update({"n":"TDS", "u":"bpm", "t":str(time.time()), "v":avgTDS})
+        msgTDS["bn"] += "TDS"
+
+        return msgTemp, msgLight, msgPH, msgWaterLevel, msgTDS
+
+
+    # Requesting to the catalog for broker's information
+    def get_broker(self):
+        try:
+            req_b = requests.get(self.catalog_url+"broker")
+            broker, port = req_b.json()["IP"], int(req_b.json()["port"])
+            print("Broker's info received\n")
+            return broker, port
+        
+        except requests.exceptions.RequestException as e:
+            print(f"Error during GET request for the broker: {e}\n\n")
 
 
     # Registering the plant and its devices  
